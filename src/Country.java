c179f3e9a6c0615f28b9fd75197e663235d3f916
whoisrmzn <rmuxtarov246@gmail.com>
2024-02-26 18:26:30 +0100

    JDBC connection (Insert and Select)

---
+++ src/Country.java
@@ -0,0 +1,15 @@
+public class Country {
+    //Tablonu yaratmag ucun
+    private String code;
+    private String name;
+    private String region;
+    private String continent;
+    //Encapculation
+    public Country(String code,String name,String continent,String region){
+        this.code=code;
+        this.name=name;
+        this.continent=continent;
+        this.region=region;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,62 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
-   public static void main(String[] args) throws SQLException {
-       Connection connection = null;
-       DbHelper helper =new DbHelper();
-       Statement statement=null;
-       ResultSet resultSet;
-       try{
+    public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
+        /**
+         *PrepareStatment--Crate a SQL to insert
+         */
+        PreparedStatement statement = null;
 
-           connection = helper.getConnection();
-           statement = connection.createStatement();
-           resultSet = statement.executeQuery("SELECT code,name,Continent,region FROM country");
-           while(resultSet.next()){
-               System.out.println(resultSet.getString("name"));
-           }
-       }catch (SQLException exception){
-           helper.showErrorMessage(exception);
+        try {
 
-       }finally {
-           connection.close();
-       }
+            connection = helper.getConnection();
+            statement = connection.prepareStatement
+                    ("insert into city (Name,CountryCode,District,Population) values('Sahil','AZE','Sahil',50000)");
+           //Start button for code||
+            statement.executeUpdate();
+            System.out.println("Info added ; ");
+        } catch (SQLException exception) {
+            helper.showErrorMessage(exception);
+
+        } finally {
+            connection.close();
+        }
+
+
+    }
+
+
+    public static void selectDemo() throws SQLException {
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
+        Statement statement = null;
+        ResultSet resultSet;
+        try {
+
+            connection = helper.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT code,name,Continent,region FROM country");
+            ArrayList<Country> countries = new ArrayList<Country>();
+            while (resultSet.next()) {
+                countries.add(new Country(
+                        resultSet.getString("Code"),
+                        resultSet.getString("Name"),
+                        resultSet.getString("Continent"),
+                        resultSet.getString("Region")));
+            }
+            System.out.println(countries.size());
+        } catch (SQLException exception) {
+            helper.showErrorMessage(exception);
+
+        } finally {
+            connection.close();
+        }
 
     }
 }
